0bda428a592326f07a246b2d51486e97475aef7c
Inferno47 <aymericgand@lacombedulionvert.fr>
2020-02-14 15:29:05 +0100

    First Commit

---
+++ csharp/TicketOfficeTest/TicketOfficeReservationInTrain.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace KataTrainReservation.TicketOfficeTest
+{
+    [TestFixture]
+    public class TicketOfficeReservationInTrain
+    {
+    
+        [Test]
+        public void reserveSeats() {
+            // TODO: write this code
+
+        }
+    }
+}
